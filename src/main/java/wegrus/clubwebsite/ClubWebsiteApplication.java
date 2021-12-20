58423ff9813ead83644e5ead118bef2f9b950afd
seonpilKim <ksp970306@gmail.com>
2021-12-20 23:10:34 +0900

    init commit

---
+++ src/main/java/wegrus/clubwebsite/ClubWebsiteApplication.java
@@ -0,0 +1,13 @@
+package wegrus.clubwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubWebsiteApplication.class, args);
+	}
+
+}
