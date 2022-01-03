882d03af2727f63fc9efaadc9b82d56c291ebdcf
seonpilKim <ksp970306@gmail.com>
2022-01-03 23:54:58 +0900

    Feat: 필요한 라이브러리 추가, 설정 클래스 구현
    - Swagger-ui 2.9.2v, querydsl, jwt 라이브러리 추가
    - PasswordEncoder, JpaQueryFactory bean 등록
    - SwaggerConfig 클래스 추가
    
    Resolve: #1

--- src/main/java/wegrus/clubwebsite/ClubWebsiteApplication.java
+++ src/main/java/wegrus/clubwebsite/ClubWebsiteApplication.java
@@ -1,8 +1,15 @@
 package wegrus.clubwebsite;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.persistence.EntityManager;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class ClubWebsiteApplication {
 
@@ -10,4 +17,13 @@ public class ClubWebsiteApplication {
 		SpringApplication.run(ClubWebsiteApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory(EntityManager entityManager){
+		return new JPAQueryFactory(entityManager);
+	}
 }
