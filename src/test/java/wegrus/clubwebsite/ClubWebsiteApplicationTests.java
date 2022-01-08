a263448de5f339605699e97a97e11fb83bd2bdeb
seonpilKim <ksp970306@gmail.com>
2022-01-08 16:17:44 +0900

    Fix: AWS 배포 관련 에러 해결
    - build.gradle에 jar { enable = false } 추가
      - 해당 명령어 추가로 [app name]-plain.jar이 더 이상 생기기 않음
    - Test 클래스에 @SpringBootTest 주석 처리
      - 아직 테스트 디렉토리에 설정 파일이 없어서 Context Load시 에러 발생
      - 임시로 주석 처리
    
    Resolve: #9

--- src/test/java/wegrus/clubwebsite/ClubWebsiteApplicationTests.java
+++ src/test/java/wegrus/clubwebsite/ClubWebsiteApplicationTests.java
@@ -3,7 +3,7 @@ package wegrus.clubwebsite;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ClubWebsiteApplicationTests {
 
 	@Test
