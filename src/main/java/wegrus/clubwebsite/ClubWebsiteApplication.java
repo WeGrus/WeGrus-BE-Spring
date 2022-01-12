b3826203a2f6200f1c2bba9b1e671ecddea8bfc0
seonpilKim <ksp970306@gmail.com>
2022-01-12 21:10:08 +0900

    Refactor: Querydsl 설정 파일 분리

--- src/main/java/wegrus/clubwebsite/ClubWebsiteApplication.java
+++ src/main/java/wegrus/clubwebsite/ClubWebsiteApplication.java
@@ -1,13 +1,10 @@
 package wegrus.clubwebsite;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import javax.persistence.EntityManager;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -20,10 +17,5 @@ public class ClubWebsiteApplication {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public JPAQueryFactory jpaQueryFactory(EntityManager entityManager){
-		return new JPAQueryFactory(entityManager);
 	}
 }
