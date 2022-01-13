52aaf1f67a66cce0dcf068de2156271d755af236
seonpilKim <ksp970306@gmail.com>
2022-01-13 20:36:54 +0900

    Style: 로그인 실패 응답 DTO 네이밍 변경

---
+++ src/main/java/wegrus/clubwebsite/dto/member/MemberSigninFailResponse.java
@@ -0,0 +1,13 @@
+package wegrus.clubwebsite.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberSigninFailResponse {
+
+    private String status;
+}
