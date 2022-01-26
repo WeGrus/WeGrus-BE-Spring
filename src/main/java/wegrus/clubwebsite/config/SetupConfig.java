4df6717426173b63ed6f4b975b1bf792e211b1d1
seonpilKim <ksp970306@gmail.com>
2022-01-26 23:36:32 +0900

    Feat: SetupConfig 수정
    - 서버를 킬 때마다 아래의 작업 수행
      - Roles 테이블 batch delete
      - MemberRoles에 존재하는 권한 JdbcTemplate batch insert

--- src/main/java/wegrus/clubwebsite/config/SetupConfig.java
+++ src/main/java/wegrus/clubwebsite/config/SetupConfig.java
@@ -2,23 +2,45 @@ package wegrus.clubwebsite.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import wegrus.clubwebsite.entity.member.MemberRoles;
-import wegrus.clubwebsite.entity.member.Role;
 import wegrus.clubwebsite.repository.RoleRepository;
 
 import javax.annotation.PostConstruct;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 @RequiredArgsConstructor
 public class SetupConfig {
 
     private final RoleRepository roleRepository;
+    private final JdbcTemplate jdbcTemplate;
 
     @PostConstruct
     private void setup() {
-        if (roleRepository.findAll().isEmpty()) {
-            for (MemberRoles role : MemberRoles.values())
-                roleRepository.save(new Role(role.name()));
-        }
+        roleRepository.deleteAllInBatch();
+
+        final List<String> roles = Arrays.stream(MemberRoles.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+
+        final String sql = "INSERT INTO ROLES (`role_name`) VALUES(?)";
+        final BatchPreparedStatementSetter pss = new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setString(1, roles.get(i));
+            }
+
+            @Override
+            public int getBatchSize() {
+                return roles.size();
+            }
+        };
+        jdbcTemplate.batchUpdate(sql, pss);
     }
 }
