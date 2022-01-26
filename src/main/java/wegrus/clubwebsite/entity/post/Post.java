d765a00412145f0a9310638a5490c252c2dbc1de
imgzon3 <imgzon3@naver.com>
2022-01-27 03:01:20 +0900

    Refactor: Boards -> Posts로 이름 변경4

--- src/main/java/wegrus/clubwebsite/entity/post/Post.java
+++ src/main/java/wegrus/clubwebsite/entity/post/Post.java
@@ -24,11 +24,11 @@ public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_id", updatable = false)
+    @Column(name = "post_id", insertable = false, updatable = false)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id", nullable = false)
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
     @OneToMany(mappedBy = "post")
