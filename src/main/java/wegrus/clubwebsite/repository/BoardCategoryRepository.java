dfd5e893aa1806623886017234958b4740f60f5b
imgzon3 <imgzon3@naver.com>
2022-01-27 06:16:49 +0900

    Fix: 오타 수정

--- src/main/java/wegrus/clubwebsite/repository/BoardCategoryRepository.java
+++ src/main/java/wegrus/clubwebsite/repository/BoardCategoryRepository.java
@@ -1,6 +1,7 @@
 package wegrus.clubwebsite.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import wegrus.clubwebsite.entity.post.BoardCategory;
 
-public interface BoardCategoryRepository extends JpaRepository<BoardRepository, Long> {
+public interface BoardCategoryRepository extends JpaRepository<BoardCategory, Long> {
 }
