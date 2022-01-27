604018af89e35b2c51e3f051691ca3ff22a3f53a
imgzon3 <imgzon3@naver.com>
2022-01-27 21:02:03 +0900

    Feat: SetupConfig 수정
    - 서버를 킬 때마다 아래의 작업 수행
      - Boards 테이블 batch delete
      - BoardCategories의 PK값 저장
      - Boards에 존재하는 권한 BoardCategories의 PK와 함께 JdbcTemplate batch insert

---
+++ src/main/java/wegrus/clubwebsite/entity/post/Boards.java
@@ -0,0 +1,16 @@
+package wegrus.clubwebsite.entity.post;
+
+public enum Boards {
+    NOTICE,         // 공지사항
+    IXPLOIT,        // 소모임 IXPLOIT
+    IGDC,           // 소모임 IGDC
+    ALGROUS,        // 소모임 ALGROUS
+    WEBGROUS,       // 소모임 WEBGROUS
+    STUDY,          // 스터디
+    INFO,           // 정보 공유 게시판
+    PROJECT,        // 프로젝트 모집 게시판
+    HOBBY,          // 취미 톡방 게시판
+    Q_A,            // 질문/답변 게시판
+    FREE,           // 자유 게시판
+    SUGGEST         // 건의 사항 게시판
+}
