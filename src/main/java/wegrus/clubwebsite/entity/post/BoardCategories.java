ecc1c88e7b33119bbb1ce44a5c613a7971a4356b
imgzon3 <imgzon3@naver.com>
2022-01-27 09:18:51 +0900

    Feat: SetupConfig 수정 - 서버를 킬 때마다 아래의 작업 수행   - Board_Categories 테이블 batch delete   - BoardCategories에 존재하는 권한 JdbcTemplate batch insert

---
+++ src/main/java/wegrus/clubwebsite/entity/post/BoardCategories.java
@@ -0,0 +1,8 @@
+package wegrus.clubwebsite.entity.post;
+
+public enum BoardCategories {
+    NOTICE,         // 공지사항
+    GROUP,          // 소모임
+    STUDY,          // 스터디
+    BOARD           // 게시판
+}
