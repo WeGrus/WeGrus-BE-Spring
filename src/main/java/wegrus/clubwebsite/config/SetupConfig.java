fe8f16c301cdbefbf44c89187ca9dbdfa3b512f8
seonpilKim <ksp970306@gmail.com>
2022-02-06 14:22:09 +0900

    Refactor: SetupConfig 리팩토링
    - 메소드 단위로 리팩토링
    - 불필요 코드 제거

--- src/main/java/wegrus/clubwebsite/config/SetupConfig.java
+++ src/main/java/wegrus/clubwebsite/config/SetupConfig.java
@@ -6,11 +6,7 @@ import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import wegrus.clubwebsite.entity.member.MemberRoles;
 import wegrus.clubwebsite.entity.post.BoardCategories;
-import wegrus.clubwebsite.entity.post.BoardCategory;
 import wegrus.clubwebsite.entity.post.Boards;
-import wegrus.clubwebsite.repository.BoardCategoryRepository;
-import wegrus.clubwebsite.repository.BoardRepository;
-import wegrus.clubwebsite.repository.RoleRepository;
 
 import javax.annotation.PostConstruct;
 import java.sql.PreparedStatement;
@@ -22,76 +18,22 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class SetupConfig {
 
-    private final RoleRepository roleRepository;
-    private final BoardCategoryRepository boardCategoryRepository;
-    private final BoardRepository boardRepository;
     private final JdbcTemplate jdbcTemplate;
 
     @PostConstruct
     private void setup() {
-        // Roles
-        roleRepository.deleteAllInBatch();
+        initTableRoles();
+        initTableBoardCategories();
+        initTableBoards();
 
-        final List<String> roles = Arrays.stream(MemberRoles.values())
-                .map(Enum::name)
-                .collect(Collectors.toList());
+    }
 
-        final String sql = "INSERT INTO ROLES (`role_name`) VALUES(?)";
-        final BatchPreparedStatementSetter pss = new BatchPreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement ps, int i) throws SQLException {
-                ps.setString(1, roles.get(i));
-            }
-
-            @Override
-            public int getBatchSize() {
-                return roles.size();
-            }
-        };
-        jdbcTemplate.batchUpdate(sql, pss);
-
-        // Board_Categories
-        boardCategoryRepository.deleteAllInBatch();
-
-        final List<String> boardCategories = Arrays.stream(BoardCategories.values())
-                .map(Enum::name)
-                .collect(Collectors.toList());
-
-        final String categorySql = "INSERT INTO BOARD_CATEGORIES (`board_category_name`) VALUES(?)";
-        final BatchPreparedStatementSetter categoryPss = new BatchPreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement ps, int i) throws SQLException {
-                ps.setString(1, boardCategories.get(i));
-            }
-
-            @Override
-            public int getBatchSize() {
-                return boardCategories.size();
-            }
-        };
-        jdbcTemplate.batchUpdate(categorySql, categoryPss);
-
-        // Boards
-        boardRepository.deleteAllInBatch();
-
-        List<Long> boardCategoryIds = boardCategoryRepository.findAll()
-                .stream()
-                .map(BoardCategory::getId)
-                .collect(Collectors.toList());
-
-        List<Long> boardCategoryIdOrders = Arrays.asList(
-                boardCategoryIds.get(0),
-                boardCategoryIds.get(1),
-                boardCategoryIds.get(1),
-                boardCategoryIds.get(1),
-                boardCategoryIds.get(1),
-                boardCategoryIds.get(2),
-                boardCategoryIds.get(3),
-                boardCategoryIds.get(3),
-                boardCategoryIds.get(3),
-                boardCategoryIds.get(3),
-                boardCategoryIds.get(3),
-                boardCategoryIds.get(3)
+    private void initTableBoards() {
+        final List<Long> boardCategoryIdOrders = Arrays.asList(
+                1L,
+                2L, 2L, 2L, 2L,
+                3L,
+                4L, 4L, 4L, 4L, 4L, 4L
         );
 
         final List<String> boards = Arrays.stream(Boards.values())
@@ -113,6 +55,45 @@ public class SetupConfig {
             }
         };
         jdbcTemplate.batchUpdate(boardSql, boardPss);
+    }
 
+    private void initTableBoardCategories() {
+        final List<String> boardCategories = Arrays.stream(BoardCategories.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+
+        final String categorySql = "INSERT INTO BOARD_CATEGORIES (`board_category_name`) VALUES(?)";
+        final BatchPreparedStatementSetter categoryPss = new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setString(1, boardCategories.get(i));
+            }
+
+            @Override
+            public int getBatchSize() {
+                return boardCategories.size();
+            }
+        };
+        jdbcTemplate.batchUpdate(categorySql, categoryPss);
+    }
+
+    private void initTableRoles() {
+        final List<String> roles = Arrays.stream(MemberRoles.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+
+        final String sql = "INSERT INTO ROLES (`role_name`) VALUES(?)";
+        final BatchPreparedStatementSetter pss = new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setString(1, roles.get(i));
+            }
+
+            @Override
+            public int getBatchSize() {
+                return roles.size();
+            }
+        };
+        jdbcTemplate.batchUpdate(sql, pss);
     }
 }
