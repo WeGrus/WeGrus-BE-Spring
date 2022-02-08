a89d804dddb3d98728b50cf46f9201a99c6f3ff1
seonpilKim <ksp970306@gmail.com>
2022-02-08 19:46:35 +0900

    Feat: SetupConfig 메소드 추가
    - initTableGroups() 추가

--- src/main/java/wegrus/clubwebsite/config/SetupConfig.java
+++ src/main/java/wegrus/clubwebsite/config/SetupConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
+import wegrus.clubwebsite.entity.group.Groups;
 import wegrus.clubwebsite.entity.member.MemberRoles;
 import wegrus.clubwebsite.entity.post.BoardCategories;
 import wegrus.clubwebsite.entity.post.Boards;
@@ -25,6 +26,7 @@ public class SetupConfig {
         initTableRoles();
         initTableBoardCategories();
         initTableBoards();
+        initTableGroups();
     }
 
     private void initTableBoards() {
@@ -91,6 +93,26 @@ public class SetupConfig {
             @Override
             public int getBatchSize() {
                 return roles.size();
+            }
+        };
+        jdbcTemplate.batchUpdate(sql, pss);
+    }
+
+    private void initTableGroups() {
+        final List<String> groups = Arrays.stream(Groups.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+
+        final String sql = "INSERT INTO igrus_groups (`group_name`) VALUES(?)";
+        final BatchPreparedStatementSetter pss = new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setString(1, groups.get(i));
+            }
+
+            @Override
+            public int getBatchSize() {
+                return groups.size();
             }
         };
         jdbcTemplate.batchUpdate(sql, pss);
