caf56313df79c81b42a0db36fd4fa5a2d2f5abc2
seonpilKim <ksp970306@gmail.com>
2022-02-11 19:29:05 +0900

    Feat: 엔티티 수정(Member, Group)
    - Member: 1:N 매핑 추가(GroupMember)
    - Group: createdDate 제거

--- src/main/java/wegrus/clubwebsite/entity/group/Group.java
+++ src/main/java/wegrus/clubwebsite/entity/group/Group.java
@@ -3,16 +3,13 @@ package wegrus.clubwebsite.entity.group;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
 @Table(name = "igrus_groups")
 public class Group {
 
@@ -23,10 +20,6 @@ public class Group {
 
     @Column(name = "group_name")
     private String name;
-
-    @Column(name = "group_create_date")
-    @CreatedDate
-    private LocalDateTime createdDate;
 
     public Group(String name) {
         this.name = name;
